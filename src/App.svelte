d001f5e0cac2d199e8674219d3b1fac8bc8f42bc
novidash <abgaoe@gmail.com>
2023-11-04 20:23:42 +0000

    Did some fiddling

--- src/App.svelte
+++ src/App.svelte
@@ -5,24 +5,24 @@
   import { onMount } from "svelte";
 
   Amplitude.init({
-      bindings: {
-        37: "prev",
-        39: "next",
-        32: "play_pause",
+    bindings: {
+      37: "prev",
+      39: "next",
+      32: "play_pause",
+    },
+    songs: [
+      {
+        name: "Risin' High (feat Raashan Ahmad)",
+        artist: "Ancient Astronauts",
+        album: "We Are to Answer",
+        url: "https://ytd-lemon.vercel.app/api/ytdl/download?v=jE28lE992d8&type=audio",
+        cover_art_url:
+          "https://i.ytimg.com/vi/Zk-nheLN0j8/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLATE9J6n4wvYkk6eY7mWiJMcl6_8Q",
       },
-      songs: [
-        {
-          name: "Risin' High (feat Raashan Ahmad)",
-          artist: "Ancient Astronauts",
-          album: "We Are to Answer",
-          url: "https://ytd-lemon.vercel.app/api/ytdl/download?v=jzaBXuWnWdY&type=audio",
-          cover_art_url:
-            "https://i.ytimg.com/vi/Zk-nheLN0j8/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLATE9J6n4wvYkk6eY7mWiJMcl6_8Q",
-        },
-      ],
-      debug: true
-    });
-    console.log(Amplitude);
+    ],
+    debug: true,
+  });
+  console.log(Amplitude);
 </script>
 
 <div
@@ -40,8 +40,8 @@
       <div
         class="absolute p-4 inset-0 flex flex-col justify-end bg-gradient-to-b from-transparent to-gray-900 backdrop backdrop-blur-5 text-white"
       >
-        <h3 class="font-bold">Super Artist</h3>
-        <span class="opacity-70">Albumtitle</span>
+        <h3 class="font-bold">【Vocaloid】ENISHI【Drumstep】</h3>
+        <span class="opacity-70">Tohou Collection Vol. 1</span>
       </div>
     </div>
     <!-- <div>
@@ -53,16 +53,16 @@
         </div>
       </div>
     </div> -->
-    <progress
-      id="amplitude-song-played-progress"
-      class="absolute h-1 bg-gray-200"
-    />
-
-    <div id="amplitude-song-slider"><div id="amplitude-track-progress" /></div>
+    <div class="relative h-1 bg-gray-200">
+      <progress
+        id="song-played-progress"
+        class="absolute h-full w-full bg-green-500 flex items-center justify-end amplitude-song-played-progress"
+      />
+    </div>
     <div
       class="flex justify-between text-xs font-semibold text-gray-500 px-4 py-2"
     >
-      <span id="amplitude-current-time"> 0:00 </span>
+      <div><span class="amplitude-current-minutes"></span>:<span class="amplitude-current-seconds"></span></div>
       <div class="flex space-x-3 p-2">
         <button class="focus:outline-none">
           <svg
@@ -82,18 +82,8 @@
           >
         </button>
         <button
-          class="amplitude-play-pause rounded-full w-8 h-8 flex items-center justify-center pl-0.5 ring-2 ring-gray-100 focus:outline-none"
-          id="amplitude-play-pause"
-          on:click={() => {
-            Amplitude.play({
-          name: "Risin' High (feat Raashan Ahmad)",
-          artist: "Ancient Astronauts",
-          album: "We Are to Answer",
-          url: "https://ytd-lemon.vercel.app/api/ytdl/download?v=jzaBXuWnWdY&type=audio",
-          cover_art_url:
-            "https://i.ytimg.com/vi/Zk-nheLN0j8/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLATE9J6n4wvYkk6eY7mWiJMcl6_8Q",
-        });
-          }}
+          class="rounded-full w-8 h-8 flex items-center justify-center pl-0.5 ring-2 ring-gray-100 focus:outline-none amplitude-play-pause"
+          id="play-pause"
         >
           <svg
             class="w-5 h-5"
@@ -123,7 +113,7 @@
           >
         </button>
       </div>
-      <span id="amplitude-audio-duration"> 0:00 </span>
+      <div><span id="amplitude-audio-duration" class="amplitude-duration-minutes"> 0 </span> : <span class="amplitude-duration-seconds"></span></div>
     </div>
     <ul class="text-xs sm:text-base divide-y border-t cursor-default">
       <li class="flex items-center space-x-3 hover:bg-gray-100">
@@ -138,8 +128,8 @@
             stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3" /></svg
           >
         </button>
-        <div class="flex-1">Artist - Title</div>
-        <div class="text-xs text-gray-400">2:58</div>
+        <div class="flex-1">Frizk - ALL MY FELLAS</div>
+        <div class="text-xs text-gray-400">2:21</div>
         <button class="focus:outline-none pr-4 group">
           <svg
             class="w-4 h-4 group-hover:text-green-600"
@@ -167,8 +157,8 @@
             stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3" /></svg
           >
         </button>
-        <div class="flex-1">Artist - Title</div>
-        <div class="text-xs text-gray-400">2:58</div>
+        <div class="flex-1">Lil Darkie - MUSIC FOR NIGGAS</div>
+        <div class="text-xs text-gray-400">3:58</div>
         <button class="focus:outline-none pr-4 group">
           <svg
             class="w-4 h-4 group-hover:text-green-600"
