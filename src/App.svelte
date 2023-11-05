6ba2f5ab1b67f1da3442088fe09983c5ab21971a
novidash <abgaoe@gmail.com>
2023-11-05 09:11:19 +0000

    Fix syntax

--- src/App.svelte
+++ src/App.svelte
@@ -115,7 +115,7 @@
           class="rounded-full w-8 h-8 flex items-center justify-center pl-0.5 ring-2 ring-gray-100 focus:outline-none amplitude-play-pause"
           id="play-pause"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" stroke-width="2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+          <svg xmlns="http://www.w3.org/2000/svg" stroke-width="2" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
           </svg>
           
